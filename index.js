ca64d7855beb322b33ea5dc3519af6ce857df981
MariellaNik <mariela.nik02@gmail.com>
2023-03-10 23:03:09 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+function initialize() {
+document.addEventListener("DOMContentLoaded", function() {
+// Declare an empty array to hold the employees
+let employees = [];
+//add prompt to display the inputted  for Employee and ask for confirmation, so the user can read the data
+const addEmployee = document.getElementById("add-employee-form");
+addEmployee.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    //function handling adding the new employee
+    const name = document.getElementById("employee-name").value;
+    const email = document.getElementById("employee-email").value;
+    const birthdate = document.getElementById("employee-db").value;
+    const salary = document.getElementById("employee-salary").value;
+
+
+    const confirmed = prompt(`Are you sure you want to add ${name} to the database?`);
+    if (!confirmed) {
+        return;               //do not proceed with adding the employee
+    }
+    
+    const employee = { // create new employee object
+        name: name,
+        email: email,
+        phone: phone,
+        birthdate: birthdate,
+        salary: salary
+    };
+
+    employees.push(employee); // add new employee object to array
+
+    //clear the form fields
+    addEmployee.reset();
+})
+
+//delete the employee
+function confirmDelete() {
+    if (confirm("Are you sure you want to delete?")) {
+      deleteRecord()
+    } else {
+
+    }
+  }
+});
+
+//do the same for tasks
+document.addEventListener("DOMContentLoaded", function() {
+  let tasks = [];
+const addTask = document.getElementById("add-task-form");
+addTask.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const title = document.getElementById("task-title").value;
+    const description = document.getElementById("task-description").value;
+    const assignee = document.getElementById("task-assignee").value;
+    const duedate = document.getElementById("task-due").value;
+
+
+    const confirmed2 = prompt(`Are you sure you want to add task with name ${title} to the database?`);
+    if (!confirmed2) {
+        return;               
+    }
+    
+    const task = { 
+        title: title,
+        description: description,
+        assigneee: assignee,
+        duedate: duedate,
+    };
+
+    tasks.push(task); 
+    addTask.reset();
+})
+});  
+
+}
+
+export { initialize };
---
+++ scores.js
@@ -0,0 +1,33 @@
+import { initialize } from './index.js';
+
+initialize();
+
+
+employees.forEach(employee => {
+    employee.taskCount = 0;
+  });
+  
+  // Increment the task counter for the assigned employee
+  tasks.forEach(task => {
+    const assignedEmployee = employees.find(employee => employee.name === task.assignee);
+    assignedEmployee.taskCount++;
+  });
+  
+  // Sort employees by task count in descending order
+  const sortedEmployees = employees.sort((a, b) => b.taskCount - a.taskCount);
+  
+  // Get the top 5 employees with the most tasks
+  const topEmployees = sortedEmployees.slice(0, 5);
+  
+  // Display the top 5 employees in the browser
+  const topEmployeesList = document.getElementById("top-employees");
+  
+  // Loop through the top 5 employees and add them as list items to the unordered list
+  for (let i = 0; i < topEmployees.length; i++) {
+    const employee = topEmployees[i];
+    const listItem = document.createElement("li");
+    listItem.textContent = `${employee.name}: ${employee.taskCount} tasks`;
+    topEmployeesList.appendChild(listItem);
+  }
+  
+  
